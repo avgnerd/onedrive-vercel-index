2fba1092555cef63f87a6f40ef5f896b24ca75e0
avgnerd <avgnerd@naver.com>
2022-02-16 11:53:24 +0900

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -14,7 +14,7 @@ module.exports = {
   icon: '/icons/128.png',
 
   // The name of your website. Present alongside your icon.
-  title: "animeraw.org",
+  title: "animeraws.org",
 
   // The folder that you are to share publicly with onedrive-vercel-index. Use '/' if you want to share your root folder.
   baseDirectory: '/Public',
